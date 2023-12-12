56240ae2eebf99b724c10648828a4929e60598b7
MikeXado <mike.gurin21@gmail.com>
2023-12-12 14:33:03 +0200

    🐛 FIX: nx tags lint error

--- apps/pills-tracker-web/app/page.tsx
+++ apps/pills-tracker-web/app/page.tsx
@@ -1,11 +1,6 @@
 import { Button } from '@pills-tracker/pills-tracker-web-ui'
 
-export default async function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
+export default function Index() {
   return (
     <div>
       <Button variant="default">Submit</Button>
